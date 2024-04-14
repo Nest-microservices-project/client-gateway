4c89f3c2bafe0a9a74b291076d8b09159f6cbdd1
younesKabiriFarah <youneskabiri17@gmial.com>
2024-04-14 16:57:43 +0200

    ApiGateway project implemented

---
+++ src/products/products.controller.ts
@@ -0,0 +1,69 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { catchError, firstValueFrom } from 'rxjs';
+import { PaginationDTO } from 'src/common';
+import { PRODUCT_SERVICE } from 'src/config';
+import { CreateProductDto, UpdateProductDto } from './dto';
+
+@Controller('products')
+export class ProductsController {
+  constructor(
+    @Inject(PRODUCT_SERVICE) private readonly productsClient: ClientProxy,
+  ) {}
+
+  @Post()
+  createProduct(@Body() createProduct: CreateProductDto) {
+    this.productsClient.send({ cmd: 'create_product' }, createProduct);
+  }
+
+  @Get()
+  findAllProduct(@Query() paginationDTO: PaginationDTO) {
+    return this.productsClient.send({ cmd: 'find_all_product' }, paginationDTO);
+  }
+
+  @Get(':id')
+  async findProduct(@Param('id') id: string) {
+    try {
+      const product = await firstValueFrom(
+        this.productsClient.send({ cmd: 'find_one_product' }, { id }),
+      );
+      return product;
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+
+  @Patch(':id')
+  updateProduct(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    return this.productsClient
+      .send({ cmd: 'update_product' }, { id, ...updateProductDto })
+      .pipe(
+        catchError((err) => {
+          throw new RpcException(err);
+        }),
+      );
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') id: string) {
+    return this.productsClient.send({ cmd: 'delete_product' }, { id }).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
+  }
+}
